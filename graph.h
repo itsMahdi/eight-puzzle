1295d0d3edd78b93c01cc231c0a6b913b8b60cc2
itsMahdi <mahdipourghasem@gmail.com>
2016-10-28 17:12:07 +0330

    starting graph.h header file

---
+++ graph.h
@@ -0,0 +1,26 @@
+// this part is for making the graph of puzzle states
+// and searching algouritjms for them
+
+#include <iostream>
+#include "puzzle_struct.h"
+using namespace std;
+
+class Graph 
+{
+private:
+	state_node *start;
+
+
+public:
+
+
+}
+
+
+
+
+
+int main()
+{
+	return 0
+}
